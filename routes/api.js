840d75f727523e9b9dc0e7bc318ad308c690325e
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-10-11 17:36:06 -0400

    Created the get and put routes in the api.js

---
+++ routes/api.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const Workout = require("../models/workouts.js");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, { $push: { excercises: body}}, {new: true})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+     
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .limit(5)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;

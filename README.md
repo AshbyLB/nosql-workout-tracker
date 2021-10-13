34d760a25a1bbf4e9e381fbbcf2422ea15f5644e
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-10-12 21:03:11 -0400

    added the README

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# nosql-workout-tracker
-Workout Tracker App
+# NoSQL Workout Tracker
+    
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+## Description
+  A wokout tracker to help you keep track of your recent workouts!
+  
+## Table of Contents
+  - [Usage](#Usage)
+  - [Usage](#Usage)
+  - [Contributing](#Contributing)
+  - [Commands](#Commands)
+  - [Screenshots](#Screenshots)
+  - [Credits](#Credits)
+  - [License](#License)
+  - [Questions](#Questions)
+  
+## Usage
+  Use this app to keep track of your most recent exercises!  Log the exercise and the app will keep track for you and also provide you with handy graphs on the Dashboard page. 
+  
+  
+## Contributing 
+  You may contact me with suggestions on additions to the project.
+  
+## Commands
+ mongo, node, nmp run seed, npm start.
+
+## Screenshots
+
+![screenshot](./public/assets/images/dash.png)
+![screenshot](./public/assets/images/fitness.png)
+
+## Credits
+  Ashby Blakely 
+  
+  ## License<br/>
+  [Link to the MIT license used](https://opensource.org/licenses/MIT)<br/> 
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+ 
+## Questions
+  [Link to GitHub Profile](https://github.com/AshbyLB)<br/>
+  Feel free to email me at Ashbyleeblakely@gmail.com if you have any questions.
--- routes/api.js
+++ routes/api.js
@@ -5,21 +5,22 @@ router.get("/api/workouts", (req, res) => {
     console.log(req.body);
     Workout.aggregate([
         {
-         $addFields: {
-            totalDuration: 
-            {$sum: "$exercises.duration"},
-            totalDistance:
-            {$sum: "$exercises.distance"},
+            $addFields: {
+                totalDuration:
+                    { $sum: "$exercises.duration" },
+                totalDistance:
+                    { $sum: "$exercises.distance" },
 
-        }}
+            }
+        }
     ])
-    .then(dbWorkout => {
-        console.log(dbWorkout);
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 router.post("/api/workouts", ({ body }, res) => {
@@ -34,8 +35,7 @@ router.post("/api/workouts", ({ body }, res) => {
 
 
 router.put("/api/workouts/:id", (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
+
     Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true })
         .then(dbWorkout => {
             console.log(dbWorkout);
@@ -50,24 +50,23 @@ router.put("/api/workouts/:id", (req, res) => {
 router.get("/api/workouts/range", (req, res) => {
     Workout.aggregate([
         {
-         $addFields: {
-            totalDuration: 
-            {$sum: "$exercises.duration"},
-                
-             totalWeight: 
-        {$sum: "$exercises.weight" },
-        
+            $addFields: {
+                totalDuration:
+                    { $sum: "$exercises.duration" },
+
+                totalWeight:
+                    { $sum: "$exercises.weight" },
+
+            }
         }
-    }
     ])
-    .then(dbWorkout => {
-        const newArr = dbWorkout.slice(-7);
-        console.log(newArr);
-        res.json(newArr);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
+        .then(dbWorkout => {
+            const newArr = dbWorkout.slice(-7);
+            res.json(newArr);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 
